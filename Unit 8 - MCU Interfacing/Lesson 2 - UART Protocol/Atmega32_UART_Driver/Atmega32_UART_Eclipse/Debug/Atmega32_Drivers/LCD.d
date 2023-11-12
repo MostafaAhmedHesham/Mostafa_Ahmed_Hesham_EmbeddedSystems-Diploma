0928a852c86b33431b767d583cd2332225502679
MostafaAhmedHesham <mostafaahmed654@gmail.com>
2023-11-13 01:06:25 +0200

    User Modification

--- Unit 8 - MCU Interfacing/Lesson 2 - UART Protocol/Atmega32_UART_Driver/Atmega32_UART_Eclipse/Debug/Atmega32_Drivers/LCD.d
+++
@@ -1,4 +0,0 @@
-Atmega32_Drivers/LCD.o Atmega32_Drivers/LCD.o: ../Atmega32_Drivers/LCD.c \
-  D:\Mostafa\C\ and\ Embedded\Embedded\ Systems\ Diploma\ KS\Unit\ 8\ -\ MCU\ Interfacing\Lesson\ 2\Assignments\Atmega32_UART_Driver\Atmega32_UART_Eclipse\Atmega32_Drivers\inc/LCD.h
-
-D:\Mostafa\C\ and\ Embedded\Embedded\ Systems\ Diploma\ KS\Unit\ 8\ -\ MCU\ Interfacing\Lesson\ 2\Assignments\Atmega32_UART_Driver\Atmega32_UART_Eclipse\Atmega32_Drivers\inc/LCD.h:
--- Unit 8 - MCU Interfacing/Lesson 2 - UART Protocol/Atmega32_UART_Driver/Atmega32_UART_Eclipse/Debug/Atmega32_Drivers/UART.d
+++
@@ -1,5 +0,0 @@
-Atmega32_Drivers/UART.o Atmega32_Drivers/UART.o:  \
- ../Atmega32_Drivers/UART.c \
-  D:\Mostafa\C\ and\ Embedded\Embedded\ Systems\ Diploma\ KS\Unit\ 8\ -\ MCU\ Interfacing\Lesson\ 2\Assignments\Atmega32_UART_Driver\Atmega32_UART_Eclipse\Atmega32_Drivers\inc/UART.h
-
-D:\Mostafa\C\ and\ Embedded\Embedded\ Systems\ Diploma\ KS\Unit\ 8\ -\ MCU\ Interfacing\Lesson\ 2\Assignments\Atmega32_UART_Driver\Atmega32_UART_Eclipse\Atmega32_Drivers\inc/UART.h:
